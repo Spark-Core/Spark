76de35d8e96b25946e1b2aa7393dcacc80f292dd
TobiasFeld22 <Tobias.feld@hotmail.com>
2017-08-24 02:05:17 +0200

    Fixed typo in ping command.

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ command = module.exports = {}
 command.name = "ping"
 command.command = function(client, message){
     var start = new Date().getTime()
-message.channel.send("pinging...").then((message) => {
+message.channel.send("Pinging...").then((message) => {
     var end = new Date().getTime()
     message.edit("Pong! | Took **" + (end - start) + "**ms");
 })
