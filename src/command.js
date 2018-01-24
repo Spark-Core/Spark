861f80b98f22b56d1d200da1616841d3dee5e5fa
Tobias Feld <tobias.feld@hotmail.com>
2018-01-24 22:55:52 +0100

    fixed addalias for future update

--- src/command.js
+++ src/command.js
@@ -6,15 +6,17 @@ module.exports = function(client) {
             this.options = options
             this.client = client
             this.aliases = []
-            client.addCommand(this.name, this)
         }
 
 
         addAlias(alias) {
-            var {aliases} = this.client.commands.get(this.name)
-            if (!aliases.includes(alias)) {
-                aliases.push(alias)
+            if (typeof alias != "string") {
+                return console.log("Incorrect alias type, use a string")
             }
+            if (this.aliases.map(i => (i.name)).indexOf == -1) {
+                return console.log("This alias was already added.")
+            }
+            this.aliases.push({name: alias, command: this})
         }
 
 
